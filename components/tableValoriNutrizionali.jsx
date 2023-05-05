9a308b3ee1e753179af38ed38f5e1bde116a2145
paolomerloiisviolamarchesini <122516969+paolomerloiisviolamarchesini@users.noreply.github.com>
2023-05-05 13:01:56 +0200

    tableValoriNutrizionali.jsx

--- components/tableValoriNutrizionali.jsx
+++ components/tableValoriNutrizionali.jsx
@@ -1,35 +1,43 @@
-import { Table } from "@nextui-org/react";
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+function Table() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Effettua la richiesta API per recuperare i dati dal database
+    fetch("http://bevanderia.altervista.org/bevandeapi/API/valori_nutrizionali/getArchiveNutritionalValues.php")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Errore nella richiesta:', error);
+      });
+  }, []);
+
   return (
-    <Table
-      aria-label="Example static collection table"
-      css={{
-        height: "auto",
-        minWidth: "100%",
-      }}
-      selectionMode="single"
-    >
-      <Table.Header>
-        <Table.Column>Id</Table.Column>
-        <Table.Column>Bibita</Table.Column>
-        <Table.Column>Grassi</Table.Column>
-        <Table.Column>Proteine</Table.Column>
-        <Table.Column>Kcal</Table.Column>
-        <Table.Column>Fibre</Table.Column>
-        <Table.Column>Categoria</Table.Column>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row key="1">
-          <Table.Cell>Tony Reichert</Table.Cell>
-          <Table.Cell>CEO</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-          <Table.Cell>Active</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
+    <table>
+      <thead>
+        <tr>
+          <th>Kcal</th>
+          <th>Fats</th>
+          <th>Proteine</th>
+          <th>Zuccheri</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.Kcal}</td>
+            <td>{item.Fats}</td>
+            <td>{item.Proteins}</td>
+            <td>{item.Sugars}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
+
+export default Table;
