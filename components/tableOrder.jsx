5bfec431b634453590ae76aab3622fd6fddbbc67
paolomerloiisviolamarchesini <122516969+paolomerloiisviolamarchesini@users.noreply.github.com>
2023-05-12 09:11:54 +0200

    tableOrder.jsx

--- components/tableOrder.jsx
+++ components/tableOrder.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Routes, useRoutes } from 'react-router-dom';
 
 function Table() {
   const [data, setData] = useState([]);
@@ -22,27 +21,35 @@ function Table() {
     <table>
       <thead>
         <tr>
-          <th>Nome cliente</th>
-          <th>Cognome cliente</th>
-          <th>Email cliente</th>
-          <th>Telefono cliente</th>
-          <th>Data</th>
+          <th>ID</th>
+          <th>Data e Ora</th>
           <th>Totale</th>
+          <th>Nome Utente</th>
+          <th>Cognome</th>
+          <th>Email</th>
+          <th>Telefono</th>
+          <th>Ritiro</th>
           <th>Prodotti</th>
-          <td>Ritiro</td>
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.data_ora}</td>
+            <td>{item.totale}</td>
             <td>{item.nome_user}</td>
             <td>{item.cognome}</td>
             <td>{item.email}</td>
             <td>{item.telefono}</td>
-            <td>{item.data_ora}</td>
-            <td>{item.totale}</td>
-            <td>{item.nome_prodotto}</td>
             <td>{item.ritiro}</td>
+            <td>
+              <ul>
+                {item.prodotti.map((prodotto, index) => (
+                  <li key={index}>{prodotto.nome}</li>
+                ))}
+              </ul>
+            </td>
           </tr>
         ))}
       </tbody>
